5ce0e45800fd141849a4ec1d84bb24047b76b34d
anagomesdesign <ana.siqueiragomes@gmail.com>
2025-01-25 17:51:44 -0600

    feat: integrate MUI theme and components, update styles and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,48 @@
 'use client';
 
+import { Button, Container, Typography, useTheme } from "@mui/material";
 
 export default function Index() {
+    const theme = useTheme();
+
+
     return (
         
-        <div>
-            <h1>Hello There </h1>
-        </div>
+        <Container style={{
+            backgroundColor: theme.palette.background.default
+        }}>
+            
+            <Typography 
+                variant="h4">
+
+                    Hello, I'm Ana! 
+            </Typography>
+
+            <Typography
+                variant="overline">
+                    
+                    About me
+            </Typography>
+            
+            <Typography
+
+                variant="body1">
+                    I'm a Senior Product Designer based in Canada with over 15 years of experience with design and tech. My goal is collaborating to  create functional, ethical and inclusive designs that align with business goals.
+
+Currently, I’m the Lead Product Designer at Spud, a grocery delivery platform based in British Columbia, Canada. My journey has also included amazing collaborations with teams at SkipTheDishes, Ubisoft, and Shopify.
+
+When I’m not designing, you’ll find me gaming, enjoying a good coffee, exploring the outdoors, or petting my cat, Mocha.
+            
+            </Typography>
+
+            <Button 
+                
+                variant="outlined">
+                    
+                    Discover My Work
+            
+            </Button> 
+
+        </Container>
     )
 }
